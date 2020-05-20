870189efa2b2a5f3a3ff8407848eaf422fbfb264
Lukewarm Cats <vmRelaxed@yahoo.com>
2020-05-20 11:34:37 +0300

    Create termcolorz.py

---
+++ frontend/termcolorz.py
@@ -0,0 +1,37 @@
+"""
+Warning, this code isn't mine.
+"""
+
+from __future__ import print_function
+import os
+import re
+
+COLORS = dict(
+        list(zip([
+            'grey',
+            'red',
+            'green',
+            'yellow',
+            'blue',
+            'magenta',
+            'cyan',
+            'white',
+            ],
+            list(range(30, 38))
+            ))
+        )
+
+COLORS_RE = '\033\[(?:%s)m' % '|'.join(['%d' % v for v in COLORS.values()])
+
+RESET = '\033[0m'
+RESET_RE = '\033\[0m'
+
+def colored(text, color=None):
+    if os.getenv('ANSI_COLORS_DISABLED') is None:
+        fmt_str = '\033[%dm%s'
+        if color is not None:
+            text = re.sub(COLORS_RE + '(.*?)' + RESET_RE, r'\1', text)
+            text = fmt_str % (COLORS[color], text)
+        return text + RESET
+    else:
+        return text
